1042e5f4682e56bbaa886f0068cb8c9fe6fa89ef
Chris Jones <cmsj@tenshu.net>
2025-07-15 18:12:03 +0100

    Initial Commit

---
+++ TableCrashTestV2/TableCrashTestV2App.swift
@@ -0,0 +1,17 @@
+//
+//  TableCrashTestV2App.swift
+//  TableCrashTestV2
+//
+//  Created by Chris Jones on 15/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct TableCrashTestV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
