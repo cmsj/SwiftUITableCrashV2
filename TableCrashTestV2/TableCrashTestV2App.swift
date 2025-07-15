67f3b76d0ef0283a434a99325c07c0c6215d03d1
Chris Jones <cmsj@tenshu.net>
2025-07-15 18:43:48 +0100

    Add instructions

--- TableCrashTestV2/TableCrashTestV2App.swift
+++ TableCrashTestV2/TableCrashTestV2App.swift
@@ -5,6 +5,16 @@
 //  Created by Chris Jones on 15/07/2025.
 //
 
+/*
+STEPS TO REPRODUCE CRASH:
+ 1. Run app
+ 2. Drag a file from Finder onto the app window. A row will appear
+ 3. Select the new row
+ 4. Hit cmd+delete, the row will be removed
+ 5. Drag a file from Finder onto the app window.
+ 6. The app just crashed
+*/
+
 import SwiftUI
 
 @main
